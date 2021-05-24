d86e1ec0b93a9168068926bff2f61ee91cec7c20
rei-01-18 <80232197+rei-01-18@users.noreply.github.com>
2021-05-24 18:53:01 +0900

    syntaxerror

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,6 +5,6 @@ environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
         jQuery: 'jquery/src/jquery'
-    })
+    }))
 
 module.exports = environment
